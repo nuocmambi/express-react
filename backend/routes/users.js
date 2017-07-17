e35c822779203848d1c5f2de928ecb8084a0bcd0
bi <nuocmambi@gmail.com>
2017-07-17 21:00:33 +0200

    handlebars

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,33 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-let users = [{
-	id: 1,
-	username: "test1"
-},
-{
-	id: 2,
-	username: "martin"
-},
-{
-	id: 3,
-	username: "dang"
-},
-{
-	id: 4,
-	username: "meg"
-}
-];
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	// Comment out this line:
-  //res.send('respond with a resource');
-
-  // And insert something like this instead:
-  res.json(users);
+  res.send('respond with a resource');
 });
 
 module.exports = router;
